e78cb3025ffa11e5b4c0b8dbc080a982095495f4
nnicholls0 <write2npn@gmail.com>
2024-04-13 15:15:00 -0700

    It generated so many god damn files

---
+++ game/src/main.rs
@@ -0,0 +1,26 @@
+extern crate sdl2;
+extern crate gl;
+
+fn main() {
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let window = video_subsystem
+        .window("Game", 900, 700)
+        .opengl() // adds open gl flag
+        .resizable()
+        .build()
+        .unwrap();
+    let gl_context = window.gl_create_context().unwrap
+
+    let mut event_pump = sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event{
+                sdl2::event::Event::Quit {..} => break 'main,
+                _ => {},
+            }
+        }
+
+        // render window contents here
+    }
+}
